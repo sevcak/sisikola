02d42fccb4978144542d01d40f1d01b778b0c02c
sevcak <lukassevc000@gmail.com>
2022-06-07 17:29:47 +0200

    made intro splash longer

--- scripts/intro.js
+++ scripts/intro.js
@@ -3,11 +3,11 @@ const splash = document.querySelector('#intro-splash');
 document.addEventListener('DOMContentLoaded', (e)=>{
     setTimeout(()=>{
         splash.classList.add('display-none');
-    }, 1000);
+    }, 2000);
 })
 
 document.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => {
         document.getElementById("intro-splash").style.display = 'none';
-    }, 3000);
+    }, 3500);
 })
